64c016d3afe0110c7c52fd12af444e4991d419e6
smenasezona <artemkiasko@gmail.com>
2024-05-15 10:22:05 +0300

    refactor AppModal component

--- src/components/appModal/AppModal.tsx
+++ src/components/appModal/AppModal.tsx
@@ -1,45 +1,42 @@
-import { Box, Modal, Typography } from '@mui/material';
+import { Box, Modal, Typography } from '@mui/material'
+import { appModalStyles } from '../../styles/appModalStyles'
 
 type Open = {
-    isOpen:boolean,
-        content: string,
+	isOpen: boolean
+	content: string
 }
 
-type SelfProps =  {
-    open:Open,
-    setOpen: (state: Open)=>void
+type AppModalProps = {
+	open: Open
+	setOpen: (state: Open) => void
 }
 
-const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+function AppModal(props: AppModalProps) {
+	const getTitle = (content: string) => {
+		return content === 'Вход' ? <span>Вход</span> : <span>Регистрация</span>
+	}
 
-function AppModal (props: SelfProps) {
-    return (
-        <Modal
-        open={props.open.isOpen}
-        onClose={()=>props.setOpen({...props.open,isOpen:!props.open.isOpen})}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal {props.open.content === 'Вход' ? <span>Вход</span> : <span>Регистрация</span>}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    )
+	const handleClose = (open: Open, setOpen: (state: Open) => void) => {
+		setOpen({ ...open, isOpen: !open.isOpen })
+	}
+
+	return (
+		<Modal
+			open={props.open.isOpen}
+			onClose={() => handleClose(props.open, props.setOpen)}
+			aria-labelledby='modal-modal-title'
+			aria-describedby='modal-modal-description'
+		>
+			<Box sx={appModalStyles}>
+				<Typography id='modal-modal-title' variant='h6' component='h2'>
+					{getTitle(props.open.content)}
+				</Typography>
+				<Typography id='modal-modal-description' sx={{ mt: 2 }}>
+					Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+				</Typography>
+			</Box>
+		</Modal>
+	)
 }
 
 export default AppModal
