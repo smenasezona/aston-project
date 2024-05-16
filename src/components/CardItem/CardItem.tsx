329bcaa52eb73e265867a30e9ea9701cf32f57cd
IvanovKS <Skitchi@mail.ru>
2024-05-16 21:40:09 +0300

    feat: add close buttons for CardItem component

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -5,10 +5,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import HighlightOffSharpIcon from '@mui/icons-material/HighlightOffSharp';
 
 function CardItem() {
   return (
-    <Card sx={{ minWidth: 150 }}>
+    <Card sx={{ minWidth: 150, position: 'relative', paddingTop: '20px' }}>
+      <Button style={{ position: 'absolute', top: '5px', right: '0px'}}>
+        <HighlightOffSharpIcon />
+      </Button>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Название
