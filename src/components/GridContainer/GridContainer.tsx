a191933db0e915dcde4d31fe0adb7ca344ede01c
IvanovKS <Skitchi@mail.ru>
2024-05-16 19:05:30 +0300

    feat: add MUI for component GridContainer

--- src/components/GridContainer/GridContainer.tsx
+++ src/components/GridContainer/GridContainer.tsx
@@ -1,9 +1,30 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
 function GridContainer() {
+  const itemsRow1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
   return (
-    <div>
-      GridContainer
-    </div>
-  )
+    <Grid sx={{ flexGrow: 1 }} container spacing={3} style={{ marginTop: '20px' }}>
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" spacing={6}>
+          {itemsRow1.map((value) => (
+            <Grid key={value} item>
+              <Paper
+                sx={{
+                  height: 200,
+                  width: 150,
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                }}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default GridContainer
