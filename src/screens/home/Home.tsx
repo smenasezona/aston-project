910162a101d6ebbbec9ac6381869beb84076865c
user42022 <andrekizilov@yandex.ru>
2024-05-19 15:05:50 +0300

    fix: remove useEffect from useAPI hook

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,12 +1,28 @@
 import { useSelector } from 'react-redux';
 import GridContainer from '../../components/GridContainer/GridContainer';
-import { Character } from '../../types/queryTypes';
-import { RootState } from '../../store/store';
+import { AppDispatch, RootState } from '../../store/store';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { filterParams } from '../../api/characterSuggestion';
+import { SearchState } from '../../store/reducers/searchReducers';
+import { returnInitialQuery } from '../../api/api';
+import { useDispatch } from 'react-redux';
+import { setCharacters, setSearchParamsAction } from '../../store/actions/searchActions';
 
 function Home() {
-  const characters = useSelector<RootState>((state) => state.search.characters) as Character[]
+  const location = useLocation()
+  const dispatch = useDispatch<AppDispatch>()
+  const search = useSelector<RootState>((state) => state.search) as SearchState
+
+  useEffect(() => {
+    const updatedParams = { ...filterParams(search.queryParams), ...returnInitialQuery(location.search) }
+    dispatch(setSearchParamsAction(filterParams(updatedParams)))
+    dispatch(setCharacters(filterParams(updatedParams)))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location])
+
   return (
-    <GridContainer characters={characters}/>
+    <GridContainer characters={search.characters}/>
   );
 }
 
--- src/utils/hooks/useAPI.tsx
+++ src/utils/hooks/useAPI.tsx
@@ -1,25 +1,15 @@
 import { useDispatch, useSelector } from "react-redux"
 import { AppDispatch, RootState } from "../../store/store"
 import { SearchState } from "../../store/reducers/searchReducers"
-import { useLocation, useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { filterParams } from "../../api/characterSuggestion"
-import { returnInitialQuery } from "../../api/api"
-import { setCharacters, setSearchParamsAction, setSuggestions } from "../../store/actions/searchActions"
+import { setSearchParamsAction, setSuggestions } from "../../store/actions/searchActions"
 import { QueryParams } from "../../types/queryTypes"
 
 export default function useAPI() {
   const dispatch = useDispatch<AppDispatch>()
   const search = useSelector<RootState>((state) => state.search) as SearchState
-  const location = useLocation()
   const navigate = useNavigate()
-
-  useEffect(() => {
-    const updatedParams = { ...filterParams(search.queryParams), ...returnInitialQuery(location.search) }
-    dispatch(setSearchParamsAction(filterParams(updatedParams)))
-    dispatch(setCharacters(filterParams(updatedParams)))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location])
 
   return {
     storedQuery: search.queryParams,
