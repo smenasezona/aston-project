faf8c77a4f829ca4081cfb764e67b36931a4ec23
smenasezona <artemkiasko@gmail.com>
2024-05-20 12:09:16 +0300

    fix problem with reloading page

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -11,6 +11,9 @@ import { useEffect } from 'react'
 import { QueryParams } from '../../types/queryTypes'
 import { setPage } from '../../store/actions/pageActions'
 import CustomPagination from '../../components/CustomPagination/CustomPagination'
+import { useNavigate } from 'react-router-dom'
+import { useRef } from 'react'
+import { useState } from 'react'
 
 function Home() {
 	const location = useLocation()
@@ -18,23 +21,38 @@ function Home() {
 	const search = useSelector<RootState>((state) => state.search) as SearchState
 
 	const page = useSelector((state: RootState) => state.page.page)
+	const navigate = useNavigate()
+	const [initialLoad, setInitialLoad] = useState(true)
 
 	useEffect(() => {
-		const updatedParams = { ...filterParams(search.queryParams), ...returnInitialQuery(location.search) }
-		dispatch(setSearchParamsAction(filterParams(updatedParams)))
-		dispatch(setCharacters(filterParams(updatedParams)))
+		if (initialLoad) {
+			const params = new URLSearchParams(location.search)
+			const pageFromURL = parseInt(params.get('page') || '1', 10)
+			dispatch(setPage(pageFromURL))
+
+			const updatedParams = { ...filterParams(search.queryParams), ...returnInitialQuery(location.search) }
+
+			dispatch(setSearchParamsAction(filterParams(updatedParams)))
+			dispatch(setCharacters(filterParams(updatedParams)))
+			setInitialLoad(false)
+		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [location])
+	}, [initialLoad, location.search])
 
 	useEffect(() => {
 		const queryParams: Partial<QueryParams> = { page: String(page) }
 		dispatch(setCharacters(queryParams))
-	}, [dispatch, page])
+		const params = new URLSearchParams(location.search)
+
+		if (params.get('page') !== String(page)) {
+			params.set('page', String(page))
+			navigate({ search: params.toString() }, { replace: true })
+		}
+	}, [page, navigate, location.search, dispatch, initialLoad])
 
 	const handlePageChange = (newPage: number) => {
 		dispatch(setPage(newPage))
 	}
-
 
 	return (
 		<>
