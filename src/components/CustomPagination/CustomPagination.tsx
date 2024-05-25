ab215d763b8816a39d55854015fccc7c2ec4c170
smenasezona <artemkiasko@gmail.com>
2024-05-25 15:54:13 +0300

    change pagination colors in dark theme

--- src/components/CustomPagination/CustomPagination.tsx
+++ src/components/CustomPagination/CustomPagination.tsx
@@ -1,5 +1,6 @@
-import { Pagination, Stack } from '@mui/material'
+import { Pagination, Stack, styled } from '@mui/material'
 import React from 'react'
+import { useTheme } from '../../context/ThemeContext'
 
 interface customPaginationProps {
 	currentPage: number
@@ -8,9 +9,25 @@ interface customPaginationProps {
 }
 
 const CustomPagination: React.FC<customPaginationProps> = (props: customPaginationProps) => {
+	const { theme } = useTheme()
+
+	const CustomPagination = styled(Pagination)(() => ({
+		'& .MuiPaginationItem-root': {
+			borderColor: theme === 'dark' ? '#717274' : '#424242',
+			color: theme === 'dark' ? '#fefefefe' : '#0f0f0f',
+			'&:hover': {
+				backgroundColor: 'rgba(0, 0, 255, 0.1)',
+			},
+			'&.Mui-selected': {
+				backgroundColor: theme === 'dark' ? '#181a1f' : '##d7d6da',
+				color: theme === 'dark' ? '#fefefefe' : '#0f0f0f',
+			},
+		},
+	}))
+
 	return (
 		<Stack spacing={2} justifyContent={'center'} alignItems={'center'} margin={'2rem'}>
-			<Pagination
+			<CustomPagination
 				count={props.pageCount || 1}
 				page={props.currentPage}
 				onChange={props.onPageChange}
