08e773e7d6721341190317569bbf88df61d7c44e
user42022 <andrekizilov@yandex.ru>
2024-05-25 17:41:39 +0300

    feat: add history layout

--- src/screens/history/History.tsx
+++ src/screens/history/History.tsx
@@ -1,8 +1,20 @@
+import { Stack, Typography } from "@mui/material"
+import ResponsiveListItem from "../../components/ResponsiveList/ResponsiveListItem"
+
 function History() {
+
   return (
-    <div>
-      History
-    </div>
+      <Stack maxWidth={"1200px"} alignItems={"center"} py={1} spacing={3} width={'80%'} mx={'auto'}>
+        <Typography variant={'h3'}>Search History</Typography>
+        <Stack width={"100%"} spacing={1}>
+          <ResponsiveListItem queryParams={{ name: "Rick Sanchez", gender: 'Male', species: 'Human', page: "1", status: 'Unknown' }} date={Date.now()}></ResponsiveListItem>
+
+          <ResponsiveListItem queryParams={{ name: "Alexander", gender: 'Male', species: 'Human', page: "99", status: 'Alive' }} date={Date.now()}></ResponsiveListItem>
+        
+          <ResponsiveListItem queryParams={{ name: "Alexsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssander", gender: 'Male', species: 'Human', page: "99", status: 'Alive' }} date={Date.now()}></ResponsiveListItem>
+        </Stack>
+      </Stack>
+
   )
 }
 
